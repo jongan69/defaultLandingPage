893a0127d3a381f282d0eb06f6a8088448d8a855
Jonathan Gan <jon@cryptocurrensea.io>
2022-09-04 12:30:21 -0400

    autoplay wtf

--- components/Landing.js
+++ components/Landing.js
@@ -3,40 +3,48 @@ import React from "react";
 import Particles from 'react-tsparticles'
 import { loadSnowPreset } from "tsparticles-preset-snow";
 
+function play() {
+  var audio = document.getElementById('a1');
+  audio.play();
+}
 class Landing extends React.Component {
 
-  
-  
+
+
   render() {
-  return (
-    
-    <div>
-    <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome
-        </h1>
-        <p className={styles.description}>
-         Congrats human, you made it to Outer Space.{' '}
-          <br></br>
-          {/* <button className={styles.code} onClick={() => playMeme()}>Enter</button> */}
-          <audio 
+    return (
+
+      <div>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome
+          </h1>
+          <p className={styles.description}>
+            Congrats human, you made it to Outer Space.{' '}
+            <br></br>
+            <button className={styles.code} onClick={play}>Enter</button>
+            <audio id='a1'>
+              <source src="/cantina.mp3" type='audio/mpeg' />
+              Your browser does not support the audio element.
+            </audio>
+            {/* <audio 
            autoPlay
            loop
           // controls 
-          src="/cantina.mp3" />
-        </p>
-    </main>
-    <Particles id="tsparticles" options={{
-        preset: "snow",
-        fullScreen: {
-          zIndex: -1
-        }
-      }} init={async (engine) => await loadSnowPreset(engine)} />
-    
+          src="/cantina.mp3" /> */}
+          </p>
+        </main>
+        <Particles id="tsparticles" options={{
+          preset: "snow",
+          fullScreen: {
+            zIndex: -1
+          }
+        }} init={async (engine) => await loadSnowPreset(engine)} />
 
-    </div>
-  )
-}
+
+      </div>
+    )
+  }
 }
 
 export default Landing;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [{
+  key: 'X-DNS-Prefetch-Control',
+  value: 'on'
+}]
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
   reactStrictMode: true,
 }
 
