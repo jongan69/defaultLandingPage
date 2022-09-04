f25afe0da5a6081e63fe1117fa1d645485d98fc3
Jonathan Gan <jon@cryptocurrensea.io>
2022-09-04 12:04:25 -0400

    first commit, thanks vercel

---
+++ components/Landing.js
@@ -0,0 +1,41 @@
+import styles from '../styles/Home.module.css'
+import React from "react";
+import Particles from 'react-tsparticles'
+import { loadSnowPreset } from "tsparticles-preset-snow";
+
+class Landing extends React.Component {
+
+  
+  
+  render() {
+  return (
+    
+    <div>
+    <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome
+        </h1>
+        <p className={styles.description}>
+         Congrats human, you made it to Outer Space.{' '}
+          <br></br>
+          {/* <button className={styles.code} onClick={() => playMeme()}>Enter</button> */}
+          <audio 
+          autoPlay
+          // controls 
+          src="/cantina.mp3" />
+        </p>
+    </main>
+    <Particles id="tsparticles" options={{
+        preset: "snow",
+        fullScreen: {
+          zIndex: -1
+        }
+      }} init={async (engine) => await loadSnowPreset(engine)} />
+    
+
+    </div>
+  )
+}
+}
+
+export default Landing;
---
+++ components/Meme.js
@@ -0,0 +1,1 @@
+// Create Unregistered Sex Offender Function
